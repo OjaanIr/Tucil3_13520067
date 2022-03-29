4c74b57817f70cf139acbd6c942cd71d005bef24
OjaanIr <farnasrozaan7@gmail.com>
2022-03-29 22:42:03 +0700

    update state space tree

--- src/stastespacetree.py
+++ src/stastespacetree.py
@@ -4,3 +4,11 @@ class StateSpaceTree:
         self.root = start_state
         self.depth = 0
         self.parent = None
+
+    # Add depth of state space tree
+    def add_depth(self):
+        self.depth += 1
+
+    # Set parent of state space tree
+    def set_parent(self, parent):
+        self.parent = parent
